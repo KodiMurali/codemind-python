92060ed8ad009ed91e85334a048a30ca4b262a7e
Murali Krishna Kodi <97437638+KodiMurali@users.noreply.github.com>
2022-05-04 11:58:12 +0530

    n=int(input()) a=chr(65) for i in range(1,n+1):     for j in range(1,n+1):         print(chr(64+i),end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,6 @@
 n=int(input())
+a=chr(65)
 for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end="")
+    for j in range(1,n+1):
+        print(chr(64+i),end=" ")
     print()
-  
-    
