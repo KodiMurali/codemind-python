1a2362887c57d0d879c5a7217fa31da59f59dd57
Murali Krishna Kodi <97437638+KodiMurali@users.noreply.github.com>
2022-05-05 06:10:09 +0530

    rows = int(input()) b = 0 for i in range(rows, 0, -1):     b += 1     for j in range(1, i + 1):         print(j, end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,7 @@
-n=int(input())
-a=chr(65)
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(chr(64+i),end=" ")
+rows = int(input())
+b = 0
+for i in range(rows, 0, -1):
+    b += 1
+    for j in range(1, i + 1):
+        print(j, end='')
     print()
