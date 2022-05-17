e4ceca11172f384fa0550090b6c3e87d0edfd728
Murali Krishna Kodi <97437638+KodiMurali@users.noreply.github.com>
2022-05-17 19:24:08 +0530

    n = int(input()) for i in range(1,n+1):     for j in range(n, 0, -1):         if(i == j):            print(i,end=" ")         else:            print(j,end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,8 @@
-rows = int(input())
-b = 0
-for i in range(rows, 0, -1):
-    b += 1
-    for j in range(1, i + 1):
-        print(j, end='')
+n = int(input())
+for i in range(1,n+1):
+    for j in range(n, 0, -1):
+        if(i == j):
+           print(i,end=" ")
+        else:
+           print(j,end=" ")
     print()
