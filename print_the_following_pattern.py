9d2ca0902f210037a2907a0fee79c049e5fbb2bf
Murali Krishna Kodi <97437638+KodiMurali@users.noreply.github.com>
2022-06-29 04:54:08 +0530

    n=int(input()) for i in range(n,0,-1):     for j in range(1,i+1):         print("%c"%(i+64),end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,5 @@
-n = int(input())
-for i in range(1,n+1):
-    for j in range(n, 0, -1):
-        if(i == j):
-           print(i,end=" ")
-        else:
-           print(j,end=" ")
+n=int(input())
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print("%c"%(i+64),end=' ')
     print()
