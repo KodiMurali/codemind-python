86c8ea198e7c4609d1d3f3f7e4411d2658aec90f
Murali Krishna Kodi <97437638+KodiMurali@users.noreply.github.com>
2022-07-16 11:12:33 +0530

    string = input() flag = True;
    #Converts the given string into lowercase  
    string = string.lower();  
       
    #Iterate the string forward and backward, compare one character at a time   
    #till middle of the string is reached  
    for i in range(0, len(string)//2):  
        if(string[i] != string[len(string)-i-1]):  
            flag = False;  
            break;  
       
    if(flag):  
        print("True");  
    else:  
        print("False");

---
+++ Palindrome_or_not.py
@@ -0,0 +1,17 @@
+string = input()  
+flag = True;   
+   
+#Converts the given string into lowercase  
+string = string.lower();  
+   
+#Iterate the string forward and backward, compare one character at a time   
+#till middle of the string is reached  
+for i in range(0, len(string)//2):  
+    if(string[i] != string[len(string)-i-1]):  
+        flag = False;  
+        break;  
+   
+if(flag):  
+    print("True");  
+else:  
+    print("False");  
